9089cefc50f1a84c81801a1797815975ca4877df
avatar92 <adelbensalem1992@gmail.com>
2019-06-21 02:24:50 +0200

    Updates

---
+++ precache-manifest.8493fe7e0b5c120ecb6b8a8565d4c576.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24182c44f50e6cac64f56424a0b70847",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "4b6aa0aa96e6ec2f29c6",
+    "url": "/movie-app/static/css/main.5162a69f.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/movie-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4b6aa0aa96e6ec2f29c6",
+    "url": "/movie-app/static/js/main.6fbe7348.chunk.js"
+  },
+  {
+    "revision": "3a5315027bacbafbe345",
+    "url": "/movie-app/static/js/runtime~main.7dd63718.js"
+  }
+]);
